c4d1e331c40faad27077c5007d13b30ff3c12afa
ray19941201 <85988181+ray19941201@users.noreply.github.com>
2024-05-09 10:56:55 +0800

    Update routes.py

--- service/routes.py
+++ service/routes.py
@@ -24,7 +24,6 @@ def health():
 ######################################################################
 @app.route("/")
 def index():
-    """Root URL response"""
     return (
         jsonify(
             name="Account REST API Service",
@@ -40,10 +39,6 @@ def index():
 ######################################################################
 @app.route("/accounts", methods=["POST"])
 def create_accounts():
-    """
-    Creates an Account
-    This endpoint will create an Account based the data in the body that is posted
-    """
     app.logger.info("Request to create an Account")
     check_content_type("application/json")
     account = Account()
@@ -60,13 +55,8 @@ def create_accounts():
 ######################################################################
 # LIST ALL ACCOUNTS
 ######################################################################
-
 @app.route("/accounts", methods=["GET"])
 def list_accounts():
-    """
-    List all Accounts
-    This endpoint will list all Accounts
-    """
     app.logger.info("Request to list Accounts")
     accounts = Account.all()
     account_list = [account.serialize() for account in accounts]
@@ -78,13 +68,8 @@ def list_accounts():
 ######################################################################
 # READ AN ACCOUNT
 ######################################################################
-
 @app.route("/accounts/<int:account_id>", methods=["GET"])
 def get_accounts(account_id):
-    """
-    Reads an Account
-    This endpoint will read an Account based the account_id that is requested
-    """
     app.logger.info("Request to read an Account with id: %s", account_id)
     account = Account.find(account_id)
     if not account:
@@ -95,13 +80,8 @@ def get_accounts(account_id):
 ######################################################################
 # UPDATE AN EXISTING ACCOUNT
 ######################################################################
-
 @app.route("/accounts/<int:account_id>", methods=["PUT"])
 def update_accounts(account_id):
-    """
-    Update an Account
-    This endpoint will update an Account based on the posted data
-    """
     app.logger.info("Request to update an Account with id: %s", account_id)
     account = Account.find(account_id)
     if not account:
@@ -114,13 +94,8 @@ def update_accounts(account_id):
 ######################################################################
 # DELETE AN ACCOUNT
 ######################################################################
-
 @app.route("/accounts/<int:account_id>", methods=["DELETE"])
 def delete_accounts(account_id):
-    """
-    Delete an Account
-    This endpoint will delete an Account based on the account_id that is requested
-    """
     app.logger.info("Request to delete an Account with id: %s", account_id)
     account = Account.find(account_id)
     if account:
