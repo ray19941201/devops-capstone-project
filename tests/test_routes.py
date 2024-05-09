a6c7768a80e9f23f171258d93e68abb16c0c7aa1
ray19941201 <85988181+ray19941201@users.noreply.github.com>
2024-05-09 23:52:44 +0800

    Update test_routes.py

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -38,7 +38,9 @@ class TestAccountService(TestCase):
         app.logger.setLevel(logging.CRITICAL)
         init_db(app)
         talisman.force_https = False
-      
+
+  # seperate line
+  
     @classmethod
     def tearDownClass(cls):
         """Runs once before test suite"""
