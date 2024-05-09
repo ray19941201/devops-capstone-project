89ecb695c502e38a3831852d6076ea61fc3916b5
ray19941201 <85988181+ray19941201@users.noreply.github.com>
2024-05-09 10:53:27 +0800

    Update routes.py

--- service/routes.py
+++ service/routes.py
@@ -68,10 +68,8 @@ def list_accounts():
     This endpoint will list all Accounts
     """
     app.logger.info("Request to list Accounts")
-    
     accounts = Account.all()
     account_list = [account.serialize() for account in accounts]
-    
     app.logger.info("Returning [%s] accounts", len(account_list))
     return jsonify(account_list), status.HTTP_200_OK
 
@@ -88,11 +86,9 @@ def get_accounts(account_id):
     This endpoint will read an Account based the account_id that is requested
     """
     app.logger.info("Request to read an Account with id: %s", account_id)
-    
     account = Account.find(account_id)
     if not account:
         abort(status.HTTP_404_NOT_FOUND, f"Account with id [{account_id}] could not be found.")
-    
     return account.serialize(), status.HTTP_200_OK
 
 
@@ -110,10 +106,8 @@ def update_accounts(account_id):
     account = Account.find(account_id)
     if not account:
         abort(status.HTTP_404_NOT_FOUND, f"Account with id [{account_id}] could not be found.")
-    
     account.deserialize(request.get_json())
     account.update()
-    
     return account.serialize(), status.HTTP_200_OK
 
 
@@ -128,18 +122,15 @@ def delete_accounts(account_id):
     This endpoint will delete an Account based on the account_id that is requested
     """
     app.logger.info("Request to delete an Account with id: %s", account_id)
-        
     account = Account.find(account_id)
     if account:
         account.delete()
-        
     return "", status.HTTP_204_NO_CONTENT
 
 
 ######################################################################
 #  U T I L I T Y   F U N C T I O N S
 ######################################################################
-
 
 def check_content_type(media_type):
     """Checks that the media type is correct"""
