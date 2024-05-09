7311a7b47b589356f68da2958b6483fc802a9cc4
ray19941201 <85988181+ray19941201@users.noreply.github.com>
2024-05-09 12:24:58 +0800

    Update routes.py

--- service/routes.py
+++ service/routes.py
@@ -54,8 +54,7 @@ def list_accounts():
 
 
 @app.route("/accounts/<int:account_id>", methods=["GET"])
-def get_accounts(account_id):
-    app.logger.info("Request to read an Account with id: %s", account_id)
+def get_accounts(account_id): app.logger.info("Request to read an Account with id: %s", account_id)
     account = Account.find(account_id)
     if not account:
         abort(status.HTTP_404_NOT_FOUND, f"Account with id [{account_id}] \
@@ -67,8 +66,7 @@ def get_accounts(account_id):
 def update_accounts(account_id):
     app.logger.info("Request to update an Account with id: %s", account_id)
     account = Account.find(account_id)
-    if not account:
-        abort(status.HTTP_404_NOT_FOUND, f"Account with id [{account_id}] \
+    if not account: abort(status.HTTP_404_NOT_FOUND, f"Account with id [{account_id}] \
               could not be found.")
         account.deserialize(request.get_json())
         account.update()
